1d192b07cfd079b8d1b60efdaa6d837e5c7bcf64
TheDummi <raydecrom@hotmail.com>
2021-02-27 12:27:15 +0100

    New save

--- src/blink.cpp
+++ src/blink.cpp
@@ -1,17 +1,50 @@
 #include <Arduino.h>
-byte RedLedPin = 8;
 
+byte redLedPin = 8;
+byte blueLedPin = 7;
+byte greenLedPin = 2;
+byte yellowLedPin = 4;
 void setup()
 {
-  pinMode(RedLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  digitalWrite(RedLedPin, HIGH);
+  // Red led on
+  digitalWrite(redLedPin, HIGH);
   delay(1000);
-  digitalWrite(RedLedPin, LOW);
+  Serial.println("RED ON");
+  // Red led off
+  digitalWrite(redLedPin, LOW);
   delay(1000);
-  Serial.println("Looped");
+  Serial.println("RED OFF");
+  // Blue pin on
+  digitalWrite(blueLedPin, HIGH);
+  delay(1000);
+  Serial.println("BLUE ON");
+  // Blue led off
+  digitalWrite(blueLedPin, LOW);
+  delay(1000);
+  Serial.println("BLUE OFF");
+  // Green led on
+  digitalWrite(greenLedPin, HIGH);
+  delay(1000);
+  Serial.println("GREEN ON");
+  // Green led off
+  digitalWrite(greenLedPin, LOW);
+  delay(1000);
+  Serial.println("GREEN OFF");
+  // Yellow led off
+  digitalWrite(yellowLedPin, HIGH);
+  delay(1000);
+  Serial.println("YELLOW ON");
+  // Yellow led off
+  digitalWrite(yellowLedPin, LOW);
+  delay(1000);
+  Serial.println("YELLOW OFF");
 }
